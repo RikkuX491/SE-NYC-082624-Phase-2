00bdd110965273211a67f5d0d9b03168414541c5
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-18 13:54:21 -0400

    Added example code from the lecture

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,9 +1,56 @@
+import {useState} from "react";
+
 function Pet({pet}){
+
+    // let counter2 = 70
+
+    // console.log(useState(0))
+    // console.log(useState(0)[0])
+    // console.log(useState(0)[1])
+
+    // let numberOfLikes = 0
+
+    // const useStateArray = useState(0)
+    // const numberOfLikes = useStateArray[0]
+    // const setNumberOfLikes = useStateArray[1]
+    // console.log(numberOfLikes)
+    // console.log(setNumberOfLikes)
+
+    const [numberOfLikes, setNumberOfLikes] = useState(0)
+    // console.log(numberOfLikes)
+    
+    // console.log(numberOfLikes)
+    // console.log(setNumberOfLikes)
+
+    const [displayAnimalType, setDisplayAnimalType] = useState(false)
+    // console.log(displayAnimalType)
+
+    // Vanilla JS example of adding an event listener (pseudocode)
+    // buttonElement.addEventListener('click', () => {
+    //     numberOfLikes++
+    //     console.log(numberOfLikes)
+    // })
+
+    function increaseNumberOfLikes() {
+        setNumberOfLikes(numberOfLikes + 1)
+        // counter2++
+        // console.log(counter2)
+    }
+
+    function toggleDisplayAnimalType(){
+        setDisplayAnimalType(displayAnimalType => {
+            return !displayAnimalType
+        })
+        // setDisplayAnimalType(displayAnimalType => {
+        //     return !displayAnimalType
+        // })
+    }
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
-            <h4 className="">{pet.name}</h4>
-            <button className="like-button">0 Likes</button>
+            <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
+            <button onClick={increaseNumberOfLikes} className="like-button">{numberOfLikes} Likes</button>
         </li>
     );
 }
--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,9 +1,39 @@
 import PetList from "./PetList";
 import pets from "../data/pets";
 
-console.log(pets)
+import {useState} from "react";
+
+console.log(pets);
 
 function PetPage(){
+
+    const [searchText, setSearchText] = useState("")
+    console.log(searchText)
+
+    function handleChange(event){
+        setSearchText(event.target.value)
+    }
+
+    const filteredPets = pets.filter(pet => {
+        // if(searchText === ""){
+        //     return true
+        // }
+        return pet.name.toUpperCase().includes(searchText.toUpperCase())
+
+        // return pet.name.toUpperCase() === searchText.toUpperCase()
+
+        // return pet.name === searchText
+
+        // else if(pet.name === searchText){
+        //     return true
+        // }
+        // else{
+        //     return false
+        // }
+    })
+
+    console.log(filteredPets)
+
     return (
         <main>
             <div className="searchbar">
@@ -12,9 +42,10 @@ function PetPage(){
                     type="text"
                     id="search"
                     placeholder="Type a name to search..."
+                    onChange={handleChange}
                 />
             </div>
-            <PetList pets={pets}/>
+            <PetList pets={filteredPets}/>
         </main>
     );
 }
