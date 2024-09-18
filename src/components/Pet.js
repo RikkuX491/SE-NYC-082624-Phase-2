d4933a276d55b67c6fbb720cc9eede189eeb7152
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-18 18:48:54 -0400

    Created starter code and content for Lecture 4

---
+++ src/components/Pet.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Pet({pet}){
+
+    const [displayAnimalType, setDisplayAnimalType] = useState(false)
+
+    function toggleDisplayAnimalType(){
+        setDisplayAnimalType(displayAnimalType => !displayAnimalType)
+    }
+
+    return (
+        <li className="pet">
+            <img src={pet.image} alt={pet.name}/>
+            <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
+            <button className="like-button">{/* pet's likes goes here */} Likes</button>
+            <button className="adopt-button">Adopt</button>
+        </li>
+    );
+}
+
+export default Pet;
