bf2ce7adfaf8ecc7cf244b8f9766ab1f2849e4c0
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-25 15:11:49 -0400

    Added example code from the lecture

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,40 +2,78 @@ import { useState } from "react";
 
 function Pet({pet, deletePet, updatePet}){
 
+    // console.log(pet)
+    // console.log(pet.id)
+
     const [displayAnimalType, setDisplayAnimalType] = useState(false)
+    const [displayForm, setDisplayForm] = useState(false)
+
+    const [formData, setFormData] = useState({
+        name: pet.name,
+        image: pet.image,
+        animal_type: pet.animal_type
+    })
 
     function toggleDisplayAnimalType(){
         setDisplayAnimalType(!displayAnimalType)
     }
 
+    function toggleDisplayForm(){
+        setDisplayForm(!displayForm)
+    }
+
     function handleLikeButtonClick(){
         const updatedPetData = {
-            ...pet,
+            // ...pet,
             likes: pet.likes + 1
         }
 
-        updatePet(updatedPetData)
+        updatePet(updatedPetData, pet.id)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+
+        const updatedPetData = {
+            // ...pet,
+            ...formData
+        }
+        
+        updatePet(updatedPetData, pet.id)
+
+        setDisplayForm(!displayForm)
     }
 
     function handleAdoptButtonClick(){
         deletePet(pet.id)
     }
 
+    function updateFormData(event){
+        // console.log("Here's the original form data:")
+        // console.log(formData)
+        // console.log("Here's a copy of that data:")
+
+        setFormData({...formData, [event.target.name]: event.target.value})
+
+        // console.log(event.target.name)
+        // console.log(event.target.value)
+    }
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
-            { true ?
+            { !displayForm ?
             <div className="button-div">
-                <button className="update-button">Update Pet</button>
+                <button onClick={toggleDisplayForm} className="update-button">Update Pet</button>
                 <button onClick={handleLikeButtonClick} className="like-button">{pet.likes} Likes</button>
                 <br/>
                 <button onClick={handleAdoptButtonClick} className="adopt-button">Adopt</button>
             </div> :
-            <form className="edit-pet">
-                <input type="text" name="name" placeholder="Pet name" />
-                <input type="text" name="image" placeholder="Image URL" />
-                <input type="text" name="animal_type" placeholder="Animal type" />
+            <form onSubmit={handleSubmit} className="edit-pet">
+                <input onChange={updateFormData} type="text" name="name" placeholder="Pet name" value={formData.name} />
+                <input onChange={updateFormData} type="text" name="image" placeholder="Image URL" value={formData.image} />
+                <input onChange={updateFormData} type="text" name="animal_type" placeholder="Animal type" value={formData.animal_type} />
                 <button type="submit">Save Changes</button>
             </form>
             }
--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -23,20 +23,66 @@ function PetPage(){
     }
 
     function deletePet(id){
-        setPets(pets.filter(pet => {
-            return pet.id !== id
-        }))
-    }
 
-    function updatePet(updatedPetData){
-        setPets(pets.map(pet => {
-            if(pet.id === updatedPetData.id){
-                return updatedPetData
+        // console.log(id)
+
+        // Optimistic rendering for DELETE request
+        // setPets(pets.filter(pet => {
+        //     return pet.id !== id
+        // }))
+
+        // fetch(`http://localhost:4000/pets/${id}`, {
+        //     method: "DELETE"
+        // })
+
+        // Pessimistic rendering for DELETE request
+        fetch(`http://localhost:4000/pets/${id}`, {
+            method: "DELETE"
+        })
+        .then(response => {
+            if(response.ok){
+                // Update state to remove the pet from the pets state
+                setPets(pets.filter(pet => {
+                    return pet.id !== id
+                }))
             }
             else{
-                return pet
+                alert(`Error: Unable to delete Pet # ${id}!`)
             }
-        }))
+        })
+    }
+
+    function updatePet(updatedPetData, id){
+
+        // console.log(updatedPetData)
+        // console.log(id)
+
+        // Pessimistic rendering for PATCH request
+        fetch(`http://localhost:4000/pets/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(updatedPetData)
+        })
+        .then(response => {
+            if(response.ok){
+                response.json().then(updatedPet => {
+                    setPets(pets.map(pet => {
+                        if(pet.id === updatedPet.id){
+                            return updatedPet
+                        }
+                        else{
+                            return pet
+                        }
+                    }))
+                })
+            }
+            else{
+                alert(`Error: Unable to update Pet # ${id}!`)
+            }
+        })
     }
 
     function addPet(newPet){
